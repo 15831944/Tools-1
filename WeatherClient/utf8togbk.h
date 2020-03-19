625ffa8bb9fc5706eb20a707e4f05be44fd1eac5
wwu <42328371+wwuhn@users.noreply.github.com>
2020-03-19 13:48:31 +0800

    Add files via upload

---
+++ WeatherClient/utf8togbk.h
@@ -0,0 +1,13 @@
+#ifndef __UTF8TOGBK_H
+#define __UTF8TOGBK_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "string.h"
+
+int SwitchToGbk(const unsigned char* pszBufIn, int nBufInLen, unsigned char* pszBufOut, int* pnBufOutLen);
+#ifdef __cplusplus
+}
+#endif
+#endif
